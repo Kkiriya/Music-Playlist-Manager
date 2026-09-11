eeb6728b08a814789d8ffd38afd6bd2051eb1998
Jean-Simon Cyr <jsimcyr@gmail.com>
2026-09-11 14:58:23 -0400

    feat: enhance filter UI and add decade filter options

--- src/main/java/com/maisonneuve/music_playlist_manager/controller/PrincipalController.java
+++ src/main/java/com/maisonneuve/music_playlist_manager/controller/PrincipalController.java
@@ -7,9 +7,23 @@ import javafx.scene.control.ComboBox;
 public class PrincipalController {
     @FXML
     private ComboBox<Genre> genreFilter;
+    @FXML
+    private ComboBox<String> decennieFilter;
 
     @FXML
     public void initialize() {
         genreFilter.getItems().setAll(Genre.values());
+
+        decennieFilter.getItems().setAll(
+          "Toutes",
+          "1970s",
+          "1980s",
+          "1990s",
+          "2000s",
+          "2010s",
+          "2020s"
+        );
+
+        decennieFilter.setValue("Toutes");
     }
 }
