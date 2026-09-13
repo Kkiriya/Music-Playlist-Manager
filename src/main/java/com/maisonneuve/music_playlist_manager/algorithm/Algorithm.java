d2e7970df58d00b2314ee53f2d07cccdd28760db
Jean-Simon Cyr <jsimcyr@gmail.com>
2026-09-13 19:06:47 -0400

    feat: implement sorting algorithms for song list management

--- src/main/java/com/maisonneuve/music_playlist_manager/algorithm/Algorithm.java
+++ src/main/java/com/maisonneuve/music_playlist_manager/algorithm/Algorithm.java
@@ -1,4 +1,14 @@
 package com.maisonneuve.music_playlist_manager.algorithm;
 
+import com.maisonneuve.music_playlist_manager.model.Song;
+
+import java.util.Comparator;
+import java.util.List;
+
 public interface Algorithm {
+    String nom();
+
+    String complexiteTheorique();
+
+    void trier(List<Song> songs, Comparator<Song> comparator);
 }
